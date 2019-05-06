15fbc96f27ef781940fdd5ce4e9babc7b6b32c1a
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-05-06 15:06:13 +0300

    Replacing FakeGenreService

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,7 +5,7 @@ import Pagination from "./common/pagination";
 import ListGroup from "./common/list-group";
 import SearchBox from "./common/search-box";
 import { getMovies } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
+import { getGenres } from "../services/genre-service";
 import { paginate } from "../utils/paginate";
 import _ from "lodash";
 
@@ -20,8 +20,9 @@ class Movies extends Component {
     sortColumn: { path: "title", order: "asc" }
   };
 
-  componentDidMount() {
-    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
+  async componentDidMount() {
+    const { data } = await getGenres();
+    const genres = [{ _id: "", name: "All Genres" }, ...data];
     this.setState({ movies: getMovies(), genres });
   }
 
