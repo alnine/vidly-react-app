f95910187a24ffdf6c44a34eeca2f97fec115591
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-05-13 17:16:28 +0300

    Redirecting after login

--- src/components/common/protectedRoute.jsx
+++ src/components/common/protectedRoute.jsx
@@ -7,7 +7,15 @@ const ProtectedRoutes = ({ component: Component, render, ...rest }) => {
     <Route
       {...rest}
       render={props => {
-        if (!authService.getCurrentUser()) return <Redirect to="/login" />;
+        if (!authService.getCurrentUser())
+          return (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: props.location }
+              }}
+            />
+          );
         return Component ? <Component {...props} /> : render(props);
       }}
     />
--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import Joi from "joi-browser";
 import Form from "./common/form";
 import auth from "./../services/auth-service";
+import authService from "./../services/auth-service";
 
 class LoginForm extends Form {
   state = {
@@ -22,7 +24,9 @@ class LoginForm extends Form {
     try {
       const { data } = this.state;
       await auth.login(data.username, data.password);
-      window.location = "/";
+
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/";
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
@@ -33,6 +37,8 @@ class LoginForm extends Form {
   };
 
   render() {
+    if (authService.getCurrentUser()) return <Redirect to="/" />;
+
     return (
       <div>
         <h1>Login</h1>
