a78a911b23c6a68006552827e39f5bf92d2b25fa
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-05-15 12:36:58 +0300

    Any logined user can delete a movie

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -37,7 +37,7 @@ class MoviesTable extends Component {
   constructor() {
     super();
     const user = authService.getCurrentUser();
-    if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+    if (user) this.columns.push(this.deleteColumn);
   }
 
   render() {
